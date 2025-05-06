c969cc9b8d83e6df41bf40907337b153d6877699
Khokhulia <a.Oleksandr.Khokhuli@Nebraska.gov>
2025-05-06 09:12:43 -0500

    Project: js test code

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -71,9 +71,9 @@ function toggleAllNavSections(sections, expanded = false) {
 function toggleMenu(nav, navSections, forceExpanded = null) {
   const expanded = forceExpanded !== null ? !forceExpanded : nav.getAttribute('aria-expanded') === 'true';
   const button = nav.querySelector('.nav-hamburger button');
-  document.body.style.overflowY = (expanded || isDesktop.matches || window.innerWidth > 1225) ? '' : 'hidden';
+  document.body.style.overflowY = (expanded || (isDesktop.matches && window.innerWidth > 1225)) ? '' : 'hidden';
   nav.setAttribute('aria-expanded', expanded ? 'false' : 'true');
-  toggleAllNavSections(navSections, expanded || isDesktop.matches || window.innerWidth > 1225 ? 'false' : 'true');
+  toggleAllNavSections(navSections, expanded || (isDesktop.matches && window.innerWidth > 1225) ? 'false' : 'true');
   button.setAttribute('aria-label', expanded ? 'Open navigation' : 'Close navigation');
   // enable nav dropdown keyboard accessibility
   const navDrops = navSections.querySelectorAll('.nav-drop');
