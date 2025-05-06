6a720a736da321b0e964a787a68dc7bc5be4d12c
Khokhulia <a.Oleksandr.Khokhuli@Nebraska.gov>
2025-05-06 09:15:39 -0500

    Project: js test code

--- blocks/header/header.js
+++ blocks/header/header.js
@@ -2,7 +2,7 @@ import { getMetadata } from '../../scripts/aem.js';
 import { loadFragment } from '../fragment/fragment.js';
 
 // media query match that indicates mobile/tablet width
-const isDesktop = window.matchMedia('(min-width: 900px)');
+const isDesktop = window.matchMedia('(min-width: 1225px)');
 
 function closeOnEscape(e) {
   if (e.code === 'Escape') {
@@ -71,9 +71,9 @@ function toggleAllNavSections(sections, expanded = false) {
 function toggleMenu(nav, navSections, forceExpanded = null) {
   const expanded = forceExpanded !== null ? !forceExpanded : nav.getAttribute('aria-expanded') === 'true';
   const button = nav.querySelector('.nav-hamburger button');
-  document.body.style.overflowY = (expanded || (isDesktop.matches && window.innerWidth > 1225)) ? '' : 'hidden';
+  document.body.style.overflowY = expanded || isDesktop.matches ? '' : 'hidden';
   nav.setAttribute('aria-expanded', expanded ? 'false' : 'true');
-  toggleAllNavSections(navSections, expanded || (isDesktop.matches && window.innerWidth > 1225) ? 'false' : 'true');
+  toggleAllNavSections(navSections, expanded || isDesktop.matches  ? 'false' : 'true');
   button.setAttribute('aria-label', expanded ? 'Open navigation' : 'Close navigation');
   // enable nav dropdown keyboard accessibility
   const navDrops = navSections.querySelectorAll('.nav-drop');
